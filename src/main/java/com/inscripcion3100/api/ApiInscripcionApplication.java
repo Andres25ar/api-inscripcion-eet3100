9a4ff1da0dadf4408be7137f18ade91eb3ab115a
Andres Arancibia Diaz <andres.arancibia.lados@gmail.com>
2026-05-23 23:20:06 -0300

    first commit

---
+++ src/main/java/com/inscripcion3100/api/ApiInscripcionApplication.java
@@ -0,0 +1,13 @@
+package com.inscripcion3100.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInscripcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInscripcionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inscripcion3100/api/ApiInscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inscripcion3100.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
